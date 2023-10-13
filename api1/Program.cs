0f676c55bc78deb5213f44a5059c12cf24f4ba00
Asus <Asus@DESKTOP-KU0AVPO>
2023-10-13 23:07:21 +0330

    Add project files.

---
+++ api1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Http.Headers;
+var client = new HttpClient(); 
+var request = new HttpRequestMessage 
+{ Method = HttpMethod.Get,
+    RequestUri = new Uri("https://alpha-vantage.p.rapidapi.com/query?interval=5min&function=TIME_SERIES_INTRADAY&symbol=MSFT&datatype=json&output_size=compact"), 
+    Headers = { { "X-RapidAPI-Key", "1fabf68df4msh551f3c49435c2f9p1e6e61jsn9324140cea3a" }, { "X-RapidAPI-Host", "alpha-vantage.p.rapidapi.com" }, }, };
+using 
+(var response = await client.SendAsync(request))
+{ response.EnsureSuccessStatusCode(); var body = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(body); }
+
